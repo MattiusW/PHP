1b15d2d3ceb7c253e38326fc1a525c11725ec366
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-29 08:25:30 +0100

    Add some html

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
     echo"Hello World!";
-
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn PHP</title>
+</head>
+<body>
+    
+    <br>
+    <button>Example</button>
+
+</body>
+</html>
