ee80d6e06001dea0b532370e07c0f81ac397deda
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-29 08:49:57 +0100

    Variable in php

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php
 
-    echo"Hello World!";
+    // String
+    $name = "Mati";
+    $games = "Games";
+    
+    // Integer
+    $age = 21;
+    $quantity = 3;
+    $total = null;
+
+    
+    // Float
+    $price = 129.99;
+    
+    // Boolean
+    $online = true;
+    $for_sale = false;
+    
+    $total = $quantity * $price;
+    
+    echo"Hello {$name}<br>";
+    echo"Price is {$price}\$<br>";
+    echo"Online status: {$online}<br>";
+    echo"Is for sale?: {$for_sale}<br>";
+
+    // Several variable
+    echo"You buy {$quantity} {$games} and you will pay \${$total} for this.";
     
 ?>
 
