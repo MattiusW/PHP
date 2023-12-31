7da9b706a9f8a92f084d8ff49bfec91e3ab09644
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-31 11:12:54 +0100

    Meozation

--- LeetCode/Fib.php
+++ LeetCode/Fib.php
@@ -1,13 +1,20 @@
 <?php
 
-function fib($n){
-    if($n <= 2)
+// meoization
+// key will be arg fn, value will be return value
+
+function fib($n, &$memo = array()){
+    if(array_key_exists($n, $memo)) 
+        return $memo[$n];
+    if($n <= 1)
         return 1;
-    return fib($n - 1) + fib($n - 2);
+    $memo[$n] = fib($n - 1, $memo) + fib($n - 2, $memo);
+    return $memo[$n];
 }
 
 echo fib(5) . "\n";
 echo fib(7) . "\n";
-echo fib(3);
+echo fib(50) . "\n";
+echo fib(70);
 
 ?> 
